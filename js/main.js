28b675165a4b5c31a2955376c657d3a03447da0d
VU HOANG HA <vuhoangha100995@gmail.com>
2016-03-05 17:06:14 +0700

    dont full music tag

--- js/main.js
+++ js/main.js
@@ -9,9 +9,9 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    var displaygenres=0;
+    var displaygenres = 0;
     $("#genres-button").click(function () {
-        if (displaygenres==0) {
+        if (displaygenres == 0) {
             $("#genres-detail").show();
             $(this).css("background", "#eee");
             displaygenres = 1;
@@ -139,15 +139,46 @@ $(document).ready(function () {
 
 $(document).ready(function () {
     var playmusic = 0;
-    var audio = document.getElementById("myTune");
-    
+    var audio = document.getElementById('myTune');
+
+    function ConvertTime(timeSecond) {
+        var hour = 0;
+        var minutes = 0;
+        var second = 0;
+        var stringTime = "";
+        hour = Math.floor(timeSecond / 3600);
+        timeSecond = timeSecond - hour * 3600;
+        minutes = Math.floor(timeSecond / 60);
+        timeSecond = timeSecond - minutes * 60;
+        second = Math.floor(timeSecond);
+        var correctSecond = "";
+        if (second < 10) {
+            correctSecond = "0" + second;
+        } else {
+            correctSecond = second;
+        }
+        if (hour == 0) {
+            stringTime = minutes + ":" + correctSecond;
+        } else {
+            stringTime = hour + ":" + minutes + ":" + correctSecond;
+        }
+        return stringTime;
+    }
+
+    audio.addEventListener('loadedmetadata', function () {
+        var lengaudio = parseInt(audio.duration);
+        var lengaudioText = ConvertTime(lengaudio);
+        document.getElementById("playmusic-end").innerHTML = lengaudioText;
+        document.getElementById("playmusic-start").innerHTML = "0:00";
+    });
+
     //event end audio
-    audio.onended = function(){
-        if(audio.loop==false){
+    audio.onended = function () {
+        if (audio.loop == false) {
             PauseMusic();
         }
     };
-    
+
     //function play music
     function PlayMusic() {
         audio.play();
@@ -155,7 +186,7 @@ $(document).ready(function () {
         $("#playButton .glyphicon-play").hide();
         $("#playButton .glyphicon-pause").show();
     }
-    
+
     //function pause music
     function PauseMusic() {
         audio.pause();
@@ -178,22 +209,68 @@ $(document).ready(function () {
         }
 
     });
-    
+
     //event click playmusic
     $("#playButton").click(function () {
         if (playmusic == 0) {
             PlayMusic();
+            intervalID = setInterval(function () {
+                document.getElementById("playmusic-start").innerHTML = ConvertTime(parseInt(audio.currentTime));
+                currentValuePlayMusic = (audio.currentTime / audio.duration) * 100;
+                document.getElementById("playmusic-process").value = currentValuePlayMusic;
+            }, 100);
         } else {
             PauseMusic();
+            clearInterval(intervalID);
         }
     });
-    
-    intervalID = setInterval(function () {
-        sliderNext();
-    }, 3000);
 
-    $(".slider-img").on("mouseover", function () {
-        clearInterval(intervalID);
+    //event click fast forward music
+    var PlaymusicLenPer = $("#playmusic-process").width() / 100;
+    $("#playmusic-process").click(function (ev) {
+        var lenCurrent_playMusic = ev.clientX - $("#playmusic-process").offset().left;
+        var Playmusic_currentPer = lenCurrent_playMusic / PlaymusicLenPer;
+
+        document.getElementById("playmusic-process").value = Playmusic_currentPer;
+        document.getElementById("myTune").currentTime = (Playmusic_currentPer / 100) * audio.duration;
+        document.getElementById("playmusic-start").innerHTML = ConvertTime(audio.currentTime);
     });
-    
+
+    var volumeLenPer = $("#volumeProBar").width() / 100;
+    $("#volumeProBar").click(function (ev_volume) {
+        var lenCurrent_volume = ev_volume.clientX - $("#volumeProBar").offset().left;
+        document.getElementById("volumeProBar").value = lenCurrent_volume / volumeLenPer;
+        document.getElementById("myTune").volume = document.getElementById("volumeProBar").value / 100;
+        if (audio.volume < 0.5) {
+            $("#volume .glyphicon-volume-down").show();
+            $("#volume .glyphicon-volume-up").hide();
+        } else {
+            $("#volume .glyphicon-volume-down").hide();
+            $("#volume .glyphicon-volume-up").show();
+        }
+    });
+
+    var oldVolume;
+    var oldVolumeLength;
+    $("#volumeButton").click(function () {
+        if (audio.volume > 0) {
+            oldVolume = audio.volume;
+            oldVolumeLength = document.getElementById("volumeProBar").value;
+            document.getElementById("myTune").volume = 0;
+            $("#volume .glyphicon-volume-down").hide();
+            $("#volume .glyphicon-volume-up").hide();
+            $("#volume .glyphicon-volume-off").show();
+        } else {
+            document.getElementById("myTune").volume = oldVolume;
+            document.getElementById("volumeProBar").value = oldVolumeLength;
+            $("#volume .glyphicon-volume-off").hide();
+            if (audio.volume < 0.5) {
+                $("#volume .glyphicon-volume-down").show();
+            } else {
+                $("#volume .glyphicon-volume-up").show();
+            }
+        }
+    });
+
+
 });
